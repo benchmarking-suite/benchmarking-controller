e324825c5c0bcb351a1b6e4c916563ed2c95f428
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2017-07-06 19:32:03 +0200

    docs generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,14 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
