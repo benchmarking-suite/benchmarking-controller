bacbe5a73834dc28c74738c070719dd4e262967d
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2017-07-08 17:16:16 +0200

    fixing RTD build

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
-# hack for ReadTheDocs builds. Since the build_manpages must be downloaded from GitHub and it is done in the same
-# requirements file
+# build_manpage comes from https://github.com/gabrielegiammatteo/build_manpage and it is used to build man page from
+# the argparse cmdline parser in benchsuite.cli. Since it is not possible to express build-time dependencies,
+# if this module is not installed in the system when installing the benchsuite.controller with pip the installation
+# would fail
 try:
     from build_manpage import build_manpage
     cmdclass = {'build_manpage': build_manpage}
 except ImportError:
     cmdclass = {}
+    print('\n#\n# WARNING! Module argparse-manpage is not installed. Generation of manpage will be skipped. To enable, '
+          'download and install the module from https://github.com/gabrielegiammatteo/build_manpage\n#\n')
 
 setup(
     name='benchsuite.controller',
