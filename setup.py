026435b54afea5eb034383763c285ff475404b6a
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2017-07-09 15:52:20 +0200

    removing dependency on argparse-manpage and importing the script directly in the soure of this project

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
+import sys
 from distutils.core import setup
 
 from setuptools import find_packages
 
+from benchsuite.manpage_builder import build_manpage
 
-# build_manpage comes from https://github.com/gabrielegiammatteo/build_manpage and it is used to build man page from
-# the argparse cmdline parser in benchsuite.cli. Since it is not possible to express build-time dependencies,
-# if this module is not installed in the system when installing the benchsuite.controller with pip the installation
-# would fail
-try:
-    from build_manpage import build_manpage
-    cmdclass = {'build_manpage': build_manpage}
-except ImportError:
-    cmdclass = {}
-    print('\n#\n# WARNING! Module argparse-manpage is not installed. Generation of manpage will be skipped. To enable, '
-          'download and install the module from https://github.com/gabrielegiammatteo/build_manpage\n#\n')
 
 setup(
     name='benchsuite.controller',
@@ -29,5 +20,5 @@ setup(
     author_email='gabriele.giammatteo@eng.it',
     description='',
     install_requires=['appdirs', 'prettytable', 'paramiko', 'apache-libcloud', 'benchsuite.core'],
-    cmdclass= cmdclass
+    cmdclass={'build_manpage': build_manpage}
 )
---
+++ src/benchsuite/manpage_builder.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# BenchmarkingSuite - Benchmarking Controller
+# Copyright 2014-2017 Engineering Ingegneria Informatica S.p.A.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Developed in the ARTIST EU project (www.artist-project.eu) and in the
+# CloudPerfect EU project (https://cloudperfect.eu/)
+
+
+
+# -*- coding: utf-8 -*-
+
+"""build_manpage command -- Generate man page from setup()"""
+
+
+#
+# FROM https://github.com/gabrielegiammatteo/build_manpage
+# Since it is not possible to use it as dependency in setup.py, we are past-and-copy it here
+#
+
+
+import datetime
+from distutils.command.build import build
+from distutils.core import Command
+from distutils.errors import DistutilsOptionError
+import optparse
+
+
+class build_manpage(Command):
+
+    description = 'Generate man page from setup().'
+
+    user_options = [
+        ('output=', 'O', 'output file'),
+        ('parser=', None, 'module path to optparser (e.g. mymod:func'),
+        ('seealso=', None, 'list of manpages to put into the SEE ALSO section (e.g. bash:1)')
+        ]
+
+    def initialize_options(self):
+        self.output = None
+        self.parser = None
+        self.seealso = None
+
+    def finalize_options(self):
+        if self.output is None:
+            raise DistutilsOptionError('\'output\' option is required')
+        if self.parser is None:
+            raise DistutilsOptionError('\'parser\' option is required')
+
+        self.ensure_string_list('seealso')
+
+        mod_name, func_name = self.parser.split(':')
+        fromlist = mod_name.split('.')
+        try:
+            mod = __import__(mod_name, fromlist=fromlist)
+            self._parser = getattr(mod, func_name)()
+        except ImportError as err:
+            raise
+        self._parser.formatter = ManPageFormatter()
+        self._parser.formatter.set_parser(self._parser)
+        self.announce('Writing man page %s' % self.output)
+        self._today = datetime.date.today()
+
+    def _markup(self, txt):
+        return txt.replace('-', '\\-')
+
+    def _write_header(self):
+        version = self.distribution.get_version()
+        appname = self.distribution.get_name()
+        ret = []
+        ret.append('.TH %s 1 %s "%s v.%s"\n' % (self._markup(appname),
+                                      self._today.strftime('%Y\\-%m\\-%d'), appname, version))
+        description = self.distribution.get_description()
+        if description:
+            name = self._markup('%s - %s' % (self._markup(appname),
+                                             description.splitlines()[0]))
+        else:
+            name = self._markup(appname)
+        ret.append('.SH NAME\n%s\n' % name)
+        synopsis = self._parser.format_usage()
+        if synopsis:
+            synopsis = synopsis.replace('%s ' % appname, '')
+            ret.append('.SH SYNOPSIS\n.B %s\n%s\n' % (self._markup(appname),
+                                                      synopsis))
+        long_desc = self.distribution.get_long_description()
+        if long_desc:
+            ret.append('.SH DESCRIPTION\n%s\n' % self._markup(long_desc))
+        return ''.join(ret)
+
+    def _write_options(self):
+        ret = ['.SH OPTIONS\n']
+        ret.append(self._parser.format_help())
+        return ''.join(ret)
+
+    def _write_seealso(self):
+        ret = []
+        if self.seealso is not None:
+            ret.append('.SH "SEE ALSO"\n')
+
+            for i in self.seealso:
+                name, sect = i.split(":")
+
+                if len(ret) > 1:
+                    ret.append(',\n')
+
+                ret.append('.BR %s (%s)' % (name, sect))
+
+        return ''.join(ret)
+
+    def _write_footer(self):
+        ret = []
+        appname = self.distribution.get_name()
+        author = '%s <%s>' % (self.distribution.get_author(),
+                              self.distribution.get_author_email())
+        ret.append(('.SH AUTHORS\n.B %s\nwas written by %s.\n'
+                    % (self._markup(appname), self._markup(author))))
+        homepage = self.distribution.get_url()
+        ret.append(('.SH DISTRIBUTION\nThe latest version of %s may '
+                    'be downloaded from\n'
+                    '.UR %s\n.UE\n'
+                    % (self._markup(appname), self._markup(homepage),)))
+        return ''.join(ret)
+
+    def run(self):
+        manpage = []
+        manpage.append(self._write_header())
+        manpage.append(self._write_options())
+        manpage.append(self._write_footer())
+        manpage.append(self._write_seealso())
+        stream = open(self.output, 'w')
+        stream.write(''.join(manpage))
+        stream.close()
+
+
+class ManPageFormatter(optparse.HelpFormatter):
+
+    def __init__(self,
+                 indent_increment=2,
+                 max_help_position=24,
+                 width=None,
+                 short_first=1):
+        optparse.HelpFormatter.__init__(self, indent_increment,
+                                        max_help_position, width, short_first)
+
+    def _markup(self, txt):
+        return txt.replace('-', '\\-')
+
+    def format_usage(self, usage):
+        return self._markup(usage)
+
+    def format_heading(self, heading):
+        if self.level == 0:
+            return ''
+        return '.TP\n%s\n' % self._markup(heading.upper())
+
+    def format_option(self, option):
+        result = []
+        opts = self.option_strings[option]
+        result.append('.TP\n.B %s\n' % self._markup(opts))
+        if option.help:
+            help_text = '%s\n' % self._markup(self.expand_default(option))
+            result.append(help_text)
+        return ''.join(result)
+
+
+build.sub_commands.append(('build_manpage', None))
